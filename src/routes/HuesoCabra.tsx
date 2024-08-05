3a203490568105521f0957c07ecbbb57ae72a99e
federico <fgiannini95@gmail.com>
2024-08-05 18:02:26 +0200

    NewNavbar

--- src/routes/HuesoCabra.tsx
+++ src/routes/HuesoCabra.tsx
@@ -3,21 +3,23 @@ import { Home } from '../pages/Home/Home';
 import { ErrorPage } from '../pages/Error/ErrorPage';
 import { Contact } from '../pages/Contact/Contact';
 import { Shop } from '../pages/Shop/Shop';
-import { NavbarHuesoCabra } from '../components/NavbarHuesoCabra/NavbarHuesoCabra';
+//import { NavbarHuesoCabra } from '../components/NavbarHuesoCabra/NavbarHuesoCabra';
 import { Taller } from '../pages/Taller/Taller';
 import { dataTrabajos } from '../data/data';
 import { ProjectDetail } from '../pages/ProjectDetail/ProjectDetail';
+import { NavHuesoCabra } from '../components/NavHuesoCabra/NavHuesoCabra';
 
 
 export const HuesoCabra = () => {
   return (
     <BrowserRouter>
-      <NavbarHuesoCabra />
+      {/* <NavbarHuesoCabra /> */}
+      <NavHuesoCabra/>
         <Routes>
           <Route path='/' element={<Home data={dataTrabajos} />} />
           <Route path='/taller' element={<Taller />} />
-          <Route path='/contacto' element={<Contact />} />
           <Route path='/tienda' element={<Shop />} />
+          <Route path='/contacto' element={<Contact />} />
           <Route path='/oneProject/:id' element={<ProjectDetail/>}/>
           <Route path='*' element={<ErrorPage />} />
         </Routes>
