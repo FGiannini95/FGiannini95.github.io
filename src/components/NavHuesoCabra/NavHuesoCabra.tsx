c1b12cbe2cc08d20047a265424f52847d3af5a6c
federico <fgiannini95@gmail.com>
2024-08-09 18:23:33 +0200

    Navbar aligned with home content

--- src/components/NavHuesoCabra/NavHuesoCabra.tsx
+++ src/components/NavHuesoCabra/NavHuesoCabra.tsx
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './NavHuesoCabra.css';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export const NavHuesoCabra = () => {
@@ -8,15 +8,17 @@ export const NavHuesoCabra = () => {
   const [expanded, setExpanded] = useState(false);
 
   return (
-    <Navbar expanded={expanded} expand="md" className="navbar p-2">
-      <Navbar.Brand as={Link} to="/" className='navbarLogo'>Huesocabra</Navbar.Brand>
-      <Navbar.Toggle aria-controls="navbarNav" onClick={()=> setExpanded(!expanded)}/> 
-      <Navbar.Collapse id="navbarNav" className="justify-content-end">
-        <Nav className="ml-auto">
-          <Nav.Link as={Link} to="/tienda" onClick={() => setExpanded(false)}>Tienda</Nav.Link>
-          <Nav.Link as={Link} to="/contacto" onClick={() => setExpanded(false)}>Contacto</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+    <Navbar expanded={expanded} expand="sm" className="navbar p-2">
+      <Container>
+        <Navbar.Brand as={Link} to="/" className='navbarLogo'>Huesocabra</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarNav" className='navbarToggle' onClick={() => setExpanded(!expanded)} />
+        <Navbar.Collapse id="navbarNav" className="justify-content-end">
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/tienda" onClick={() => setExpanded(false)}>Tienda</Nav.Link>
+            <Nav.Link as={Link} to="/contacto" onClick={() => setExpanded(false)}>Contacto</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   )
 }
