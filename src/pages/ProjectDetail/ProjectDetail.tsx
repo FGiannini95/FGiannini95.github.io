e2392447f341139c861c5098a64349b05ccb90c3
federico <fgiannini95@gmail.com>
2024-08-12 07:50:43 +0200

    New structure and updating data

--- src/pages/ProjectDetail/ProjectDetail.tsx
+++ src/pages/ProjectDetail/ProjectDetail.tsx
@@ -1,35 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Carousel } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { dataTrabajos } from '../../data/data';
 import './ProjectDetails.css'
 
 export const ProjectDetail: React.FC = () => {
   const { id } = useParams();
   const project = dataTrabajos.find((elem) => elem.id === Number(id));
+  const [selectedImage, setSelectedImage] = useState(project?.images[0]);
 
   if (!project) {
     return <div>Project not found</div>;
   }
 
   return (
-    <section className="p-5">
-      <h1 className='font-weigh'>{project.title}</h1>
-      <p>{project.description}</p>
-      <p>{project.material}</p>
-      <Carousel
-        className="project-carousel"
-        interval={2000}
-        indicators={false}
-        nextIcon={<span className="carousel-control-next-icon" aria-hidden="true" />}
-        prevIcon={<span className="carousel-control-prev-icon" aria-hidden="true" />}
-      >
+    <Container className='pt-5'>
+      <Row>
+        <Col xs={12} md={6}>
+          <h1 className='font-weigh'>{project.title}</h1>
+          <p>{project.description}</p>
+          <p>{project.material}</p>
+          <p>{project.medida}</p>
+          <p>{project.peso}</p>
+        </Col>
+        <Col xs={12} md={6}>
+          <img className="d-block project-image w-50" src={`/assets/${project.carpeta}/${selectedImage}`} alt={project.title} />
+        </Col>
+      </Row>
+      <Row className="mt-4">
         {project.images.map((image, index) => (
-          <Carousel.Item key={index} className="carousel-item-custom">
-            <img className="d-block project-image w-50" src={`/assets/${project.carpeta}/${image}`} alt={`Slide ${index}`} />
-          </Carousel.Item>
+          <Col xs={3} md={2} key={index} className="mb-3">
+            <img
+              className={`thumbnail-image w-100 ${selectedImage === image ? 'selected-thumbnail' : ''}`}
+              src={`/assets/${project.carpeta}/${image}`}
+              alt={`Thumbnail ${index}`}
+              onClick={() => setSelectedImage(image)}
+            />
+          </Col>
         ))}
-      </Carousel>
-    </section>
+      </Row>
+    </Container>
   );
 }
