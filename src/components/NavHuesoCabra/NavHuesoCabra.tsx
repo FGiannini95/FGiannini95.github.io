320381d1ea779f89177dfe0e7ffeeb2a3f3fc987
federico <fgiannini95@gmail.com>
2024-08-12 17:01:12 +0200

    Fixing errors

--- src/components/NavHuesoCabra/NavHuesoCabra.tsx
+++ src/components/NavHuesoCabra/NavHuesoCabra.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './NavHuesoCabra.css';
 import { Nav, Navbar, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Container, Row, Col, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { MdEmail } from "react-icons/md";
