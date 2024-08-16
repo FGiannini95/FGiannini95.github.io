0e75c63f1ea856357eae0b72e151d6a40ff818a4
federico <fgiannini95@gmail.com>
2024-08-16 20:04:49 +0200

    new deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://FGiannini95.github.io/HuesoCabra/"
+  base: "https://FGiannini95.github.io/huesocabra"
 })
