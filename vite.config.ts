de34be66e0bf5ad0a3219e88ce762c252a0467a4
federico <fgiannini95@gmail.com>
2024-08-16 19:47:17 +0200

    New config

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Configura la carpeta de salida
-    rollupOptions: {
-      external: ['bootstrap/dist/css/bootstrap.min.css'], // Externaliza Bootstrap
-    },
-  },
+  base: "https://fgiannini95.github.io/huesocabra/"
 })
