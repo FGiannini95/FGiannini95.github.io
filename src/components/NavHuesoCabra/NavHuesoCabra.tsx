749e7182233420622b435cf57071e6b49fca0aec
federico <fgiannini95@gmail.com>
2024-08-23 07:45:22 +0200

    Adding sticky to navbar

--- src/components/NavHuesoCabra/NavHuesoCabra.tsx
+++ src/components/NavHuesoCabra/NavHuesoCabra.tsx
@@ -8,7 +8,7 @@ export const NavHuesoCabra = () => {
   const [expanded, setExpanded] = useState(false);
 
   return (
-    <Navbar expanded={expanded} expand="sm" className="navbar p-2">
+    <Navbar expanded={expanded} expand="sm" className="navbar sticky-top p-2">
       <Container>
         <Navbar.Brand as={Link} to="/" className='navbarLogo'>Huesocabra</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarNav" className='navbarToggle' onClick={() => setExpanded(!expanded)} />
