f6739f750059820ae574386bdc5ec4932a4bf1e9
federico <fgiannini95@gmail.com>
2024-10-25 13:15:03 +0200

    New icon for the menu

--- src/components/NavHuesoCabra/NavHuesoCabra.tsx
+++ src/components/NavHuesoCabra/NavHuesoCabra.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import './NavHuesoCabra.css';
 import { Nav, Navbar, Container } from 'react-bootstrap';
+import { FiMenu } from "react-icons/fi";
+
 import { Link } from 'react-router-dom';
 
 export const NavHuesoCabra = () => {
@@ -13,7 +15,14 @@ export const NavHuesoCabra = () => {
         <Navbar.Brand as={Link} to="/" className="navbarLogo">
           <h3 style={{ margin: 0, fontSize: 'inherit' }}>HUESOCABRA</h3>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarNav" className="navbarToggle" onClick={() => setExpanded(!expanded)} />
+        <Navbar.Toggle
+          aria-controls="navbarNav"
+          className="navbarToggle"
+          onClick={() => setExpanded(!expanded)}
+        >
+          <FiMenu size={24} />
+        </Navbar.Toggle>
+
         <Navbar.Collapse id="navbarNav" className="justify-content-end">
           <Nav className="ml-auto">
             <Nav.Link as={Link} to="/contacto" onClick={() => setExpanded(false)} className="navbarLogo">
@@ -23,6 +32,5 @@ export const NavHuesoCabra = () => {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-
-  )
+  );
 }
