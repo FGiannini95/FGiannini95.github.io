688da4c5b8917fe2497310fc47a47959b8f132eb
federico <fgiannini95@gmail.com>
2024-10-25 07:46:14 +0200

    New horizontal line

---
+++ src/components/HorizontalLine/HorizontalLine.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container } from 'react-bootstrap';
+
+
+export const HorizontalLine = () => {
+  return (
+    <Container>
+      <div style={{ borderTop: '3px solid rgb(17, 100, 180)', width: '100%', margin: '0 auto' }}></div>
+    </Container>
+  )
+}
